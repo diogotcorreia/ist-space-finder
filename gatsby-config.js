10353f5f9fdeb550008fac5651f6dcdc9cab1b70
Diogo Correia <me@diogotc.com>
2020-10-05 20:14:27 +0100

    Add PWA and offline support
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,25 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-source-ist-spaces`],
+  plugins: [
+    `gatsby-source-ist-spaces`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "IST Space Finder",
+        short_name: "Space Finder",
+        start_url: "/",
+        background_color: "#202124",
+        theme_color: "#009de0",
+        display: "standalone",
+        icon: "src/images/ist_logo.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`],
+      },
+    },
+  ],
 }
