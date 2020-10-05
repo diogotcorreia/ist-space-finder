7b9545d7cd5b8f41af7cf672e966e7b2601cf39d
Diogo Correia <me@diogotc.com>
2020-10-05 23:55:11 +0100

    Style SpaceItem
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- src/components/SpaceItem.jsx
+++ src/components/SpaceItem.jsx
@@ -1,14 +1,71 @@
 import React from "react"
+import styled from "styled-components"
+import Link from "./Link"
 
 const SpaceItem = ({ node }) => {
   const { istId, name, type, path } = node
 
   return (
-    <div>
-      <p>{`${type} ${name} (${istId})`}</p>
-      <p>{path.join(" > ")}</p>
-    </div>
+    <StyledRoot>
+      <p>
+        <SpaceTypeBadge>{formatType(type)}</SpaceTypeBadge>
+        {` ${name} `}
+        <SpaceIstID id={istId} />
+      </p>
+      <SpacePath path={path} />
+    </StyledRoot>
   )
 }
+
+const SpacePath = ({ path }) => {
+  return path
+    .map((pathItem, i) => (
+      <span key={i}>
+        <SpaceTypeBadge>{pathItem.split(" ")[0]}</SpaceTypeBadge>
+        {` ${pathItem.split(" ").slice(1).join(" ")}`}
+      </span>
+    ))
+    .reduce((prev, curr) => [prev, ` ➧ `, curr])
+}
+
+const formatType = str =>
+  `${str.charAt(0).toUpperCase()}${str
+    .slice(1)
+    .toLowerCase()
+    .replace("_", " ")}`
+
+const StyledRoot = styled.div`
+  padding-bottom: 8px;
+  border-bottom: solid 1px #454545;
+`
+
+const colorMap = {
+  Campus: "#F39A27",
+  Building: "#579ABE",
+  Floor: "#03C03C",
+  Room: "#C23B23",
+  "Room subdivision": "#976ED7",
+}
+
+const SpaceTypeBadge = styled.span`
+  background-color: ${props => colorMap[props.children]};
+  border-radius: 10px;
+  padding: 0 5px;
+`
+const SpaceIstID = ({ id }) => {
+  return (
+    <Link
+      href={`https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/${id}/blueprint`}
+      target="_blank"
+      rel="noopener"
+    >
+      <SpaceIstIDStyle>({id})</SpaceIstIDStyle>
+    </Link>
+  )
+}
+
+const SpaceIstIDStyle = styled.span`
+  color: #9b9b9b;
+`
 
 export default SpaceItem
