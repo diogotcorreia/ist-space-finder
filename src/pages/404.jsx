848cb8eae2c31dc2406d6d00173c9fb92c26730c
Diogo Correia <me@diogotc.com>
2020-10-06 09:53:52 +0100

    Add 404 not found page
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ src/pages/404.jsx
@@ -0,0 +1,27 @@
+import { Link } from "gatsby"
+import React from "react"
+import styled from "styled-components"
+import Layout from "../components/Layout"
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <CenteredDiv>
+        <h2>404 Not found</h2>
+        <StyledLink to="/">Go Home &#8594;</StyledLink>
+      </CenteredDiv>
+    </Layout>
+  )
+}
+
+const CenteredDiv = styled.div`
+  text-align: center;
+  flex: 1 1 auto;
+`
+
+const StyledLink = styled(Link)`
+  color: #009de0;
+  text-decoration: none;
+`
+
+export default NotFoundPage
